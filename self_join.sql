6ad24f68c2034f7f66e59a1e96c21f02fee1b639
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-04 10:45:23 +0100

    Add self join

---
+++ self_join.sql
@@ -0,0 +1,35 @@
+1. How many stops are in the database.
+SELECT COUNT(name)
+FROM stops
+
+2. Find the id value for the stop 'Craiglockhart'
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
+
+3. Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id, name
+FROM stops
+  JOIN route
+  ON id = stop
+WHERE num = '4' AND company = 'LRT';
+
+4. 
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2
+
+5. 
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+6.
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
